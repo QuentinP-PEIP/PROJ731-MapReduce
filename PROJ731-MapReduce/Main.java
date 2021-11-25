c1abb5cbeabf6d4e9935a632e07aef17787fa0ca
Souchon <72502457+romainsouchon@users.noreply.github.com>
2021-11-25 15:30:58 +0100

    Travail romain

--- PROJ731-MapReduce/Main.java
+++ PROJ731-MapReduce/Main.java
@@ -3,7 +3,15 @@ public class Main {
         String nom_fichier = "Hamilton.txt";
         Fichier fichier = new Fichier(nom_fichier);
         String partie = fichier.texte_divise.get(0);
+
         Map n = new Map(partie);
+        Shuffle s = new Shuffle(n.mot);
+        //s.affiche();
+
+        Reduce r = new Reduce(s.motreunis);
+
+        r.afficher();
+
 
         
 
